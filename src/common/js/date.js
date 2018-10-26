00b63acd9a729eed3f58f81e1aa9ea66491a84eb
StartFromThird <hzqweixin@163.com>
2018-10-26 23:57:15 +0800

    实现商品评论显示
    依据类型显示对应评论，评论为空时显示“暂无评论”，正确格式显示评论时间。解决点击+-商品不对的问题

---
+++ src/common/js/date.js
@@ -0,0 +1,25 @@
+export function formatDate (date, fmt) {
+  // yyyy 替换为年，依据yyyy长度判断剪切多少
+  if (/(y+)/.test(fmt)) {
+    fmt = fmt.replace(RegExp.$1, (date.getFullYear() + '').substr(4 - RegExp.$1.length))
+  }
+  // 替换月日时分秒
+  let o = {
+    'M+': date.getMonth() + 1,
+    'd+': date.getDate(),
+    'h+': date.getHours(),
+    'm+': date.getMinutes(),
+    's+': date.getSeconds()
+  }
+  for (let k in o) {
+    if (new RegExp(`(${k})`).test(fmt)) {
+      let str = o[k] + ''
+      fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? str : padLeftZero(str))
+    }
+  }
+  return fmt
+}
+// 数字补零
+function padLeftZero (str) {
+  return ('00' + str).substr(str.length)
+}
--- src/components/goodsdetail/GoodsDetail.vue
+++ src/components/goodsdetail/GoodsDetail.vue
@@ -1,6 +1,6 @@
 <template>
 <slide-left-animation>
-  <div class="detailWrapper" ref="detailWrapper"  v-show="isShow">
+  <div class="detailWrapper" ref="detailWrapper"  v-if="isShow">
     <div class="GoodsDetail">
       <div class="imgheader">
         <img :src="selectedGood.image" >
@@ -35,7 +35,28 @@
       <div class="splitBox"></div>
       <div class="content clearfloat">
         <h1 class="title">商品评价</h1>
+        <rating-select :ratings="selectedGood.ratings" :desc="desc"
+        :selectType="selectType" @changeType="changeSelectType"
+        :onlyContent="onlyContent" @changeOnly="changeOnlyContent"></rating-select>
+        <div class="rating-content">
+          <ul ref="selectedRatings">
+            <li class="rating-item border-1px" v-for="rating in selectedGood.ratings" :key="rating.rateTime"
+              v-show="needShow(rating.rateType, rating.text)">
+              <div class="user">
+                <span class="name">{{rating.username}}</span>
+                <img :src="rating.avatar">
+              </div>
+              <div class="time">{{showDate(rating.rateTime)}}</div>
+              <div class="detail">
+                <span :class="{'icon-thumb_up':rating.rateType===0, 'icon-thumb_down':rating.rateType===1}"></span>
+                <span class="text">{{rating.text}}</span>
+              </div>
+            </li>
+          </ul>
+          <div class="no-rating" v-show="isEmpty">暂无评论</div>
+        </div>
       </div>
+
     </div>
   </div>
 </slide-left-animation>
@@ -45,12 +66,16 @@
 import Vue from 'vue'
 import SlideLeftAnimation from '../animate/SlideLeftAnimation'
 import CartControl from '../cartcontrol/CartControl'
+import RatingSelect from '../ratingselect/ratingSelect'
 import BScroll from 'better-scroll'
+import {formatDate} from '../../common/js/date'
+const ALL = 2
 export default {
   name: 'GoodsDetail',
   components: {
     SlideLeftAnimation,
-    CartControl
+    CartControl,
+    RatingSelect
   },
   props: {
     selectedGood: {
@@ -59,12 +84,23 @@ export default {
   },
   data () {
     return {
-      isShow: false
+      isShow: false,
+      desc: {
+        all: '全部',
+        positive: '推荐',
+        negative: '吐槽'
+      },
+      selectType: ALL,
+      onlyContent: false,
+      isEmpty: false
     }
   },
   methods: {
+    // 商品详情显示及selectType，onlyContent，scroll初始化
     show () {
       this.isShow = true
+      this.selectType = ALL
+      this.onlyContent = false
       this.$nextTick(() => {
         if (!this.scroll) {
           this.detailWrapper = new BScroll(this.$refs.detailWrapper, {
@@ -76,18 +112,69 @@ export default {
         }
       })
     },
+    // 点击左上角隐藏商品详情
     hideGoodsDetail () {
       this.isShow = false
     },
+    // 第一次添加商品 给selectedGood新加count属性且设为1
     addFirstGoods (event) {
       if (!event._constructed) {
         return
       }
       Vue.set(this.selectedGood, 'count', 1)
-      this.isFirst = false
+    },
+    // 更改查看 i 类评价
+    changeSelectType (i) {
+      this.selectType = i
+      this.$nextTick(() => {
+        this.detailWrapper.refresh()
+        this.ratingHeight()
+      })
+    },
+    // 更改是否 只看有内容评价
+    changeOnlyContent (i) {
+      this.onlyContent = i
+      this.$nextTick(() => {
+        this.detailWrapper.refresh()
+        this.ratingHeight()
+      })
+    },
+    // 判断是否显示该条评论
+    needShow (type, text) {
+      // 评论是否有内容
+      let isContent = text.length > 0
+      if (this.onlyContent && !isContent) {
+        return false
+      } else {
+        if (this.selectType === ALL) {
+          return true
+        } else if (this.selectType === type) {
+          return true
+        } else {
+          return false
+        }
+      }
+    },
+    // 计算显示评价时间
+    showDate (time) {
+      let date = new Date(time)
+      return formatDate(date, 'yyyy-MM-dd hh:mm')
+    },
+    // 依据评论ul的高度判断评论是否为空
+    ratingHeight () {
+      let dom = this.$refs.selectedRatings
+      if (dom) {
+        let h = dom.clientHeight
+        if (h < 10) {
+          this.isEmpty = true
+        } else {
+          this.isEmpty = false
+        }
+      }
     }
   },
   computed: {
+    // 是否显示cartcontrol还是显示加入购物车
     isControl () {
       if (this.selectedGood.count > 0) {
         return true
@@ -95,94 +182,137 @@ export default {
         return false
       }
     }
+  },
+  mounted () {
+    this.ratingHeight()
   }
-
 }
 </script>
 
 <style lang="stylus" scoped>
-  .detailWrapper
-    position fixed
-    top 0
-    bottom 3rem /* 48/16 */
+@import "../../common/stylus/mixin.styl"
+.detailWrapper
+  position fixed
+  top 0
+  bottom 3rem /* 48/16 */
+  width 100%
+  background #fff
+  z-index 30
+  overflow hidden
+  .imgheader
+    position relative
     width 100%
-    background #fff
-    z-index 30
-    overflow hidden
-    .imgheader
-      position relative
+    height 0
+    padding-bottom 100%
+    background #fcf
+    img
+      position absolute
+      top 0
+      // left 0
       width 100%
-      height 0
-      padding-bottom 100%
-      background #fcf
-      img
-        position absolute
-        top 0
-        // left 0
-        width 100%
-        height 100%
-      .back
-        position absolute
-        top 10px
-        left 10px
-        background rgba(255, 255, 255, 0.2)
-        width 40px
+      height 100%
+    .back
+      position absolute
+      top 10px
+      left 10px
+      background rgba(255, 255, 255, 0.2)
+      width 40px
+      height 40px
+      border-radius 50%
+      .icon-arrow_lift
         height 40px
-        border-radius 50%
-        .icon-arrow_lift
-          height 40px
-          line-height 40px
-          text-align center
-          font-size 20px
-          color #fff
-    .content
-      padding 18px
-      background #fcc
-      .goodsname
-        line-height 14px
+        line-height 40px
+        text-align center
+        font-size 20px
+        color #fff
+  .content
+    padding 18px
+    // background #fcc
+    .goodsname
+      line-height 14px
+      font-size 14px
+      font-weight 700
+      color rgb(7, 17, 27)
+      margin-bottom 8px
+    .sell
+      margin-bottom 18px
+      line-height 10px
+      font-size 10px
+      color rgb(147, 153, 159)
+      span
+        margin-right 12px
+    .price
+      float left
+      line-height 24px
+      font-weight 700
+      .now
+        margin-right 8px
         font-size 14px
-        font-weight 700
-        color rgb(7, 17, 27)
-        margin-bottom 8px
-      .sell
-        margin-bottom 18px
-        line-height 10px
+        color rgb(240, 20, 20)
+      .old
         font-size 10px
         color rgb(147, 153, 159)
-        span
-          margin-right 12px
-      .price
-        float left
+        text-decoration line-through
+    .rightAdd
+      float right
+      .addToCart
+        box-sizing border-box
+        height 24px
+        padding 0 12px
         line-height 24px
-        font-weight 700
-        .now
-          margin-right 8px
-          font-size 14px
-          color rgb(240, 20, 20)
-        .old
-          font-size 10px
-          color rgb(147, 153, 159)
-          text-decoration line-through
-      .rightAdd
-        float right
-        .addToCart
-          box-sizing border-box
-          height 24px
-          padding 0 12px
-          line-height 24px
-          font-size 10px
-          color #fff
-          border-radius 12px
-          background rgb(0, 160, 220)
-      .title
-        margin-bottom 6px
-        line-height 14px
-        font-size 14px
-        color rgb(7, 17, 27)
-      .info
-        line-height 24px
+        font-size 10px
+        color #fff
+        border-radius 12px
+        background rgb(0, 160, 220)
+    .title
+      margin-bottom 6px
+      line-height 14px
+      font-size 14px
+      color rgb(7, 17, 27)
+    .info
+      line-height 24px
+      font-size 12px
+      color rgb(77, 85, 93)
+      font-weight 200
+    .rating-content
+      ul
+        background #fff
+        .rating-item
+          padding 16px 0
+          border-1px(rgba(7, 17, 27, 1))
+          &:after
+            display block
+            height 0
+            clear both
+          .time
+            line-height 12px
+            font-size 10px
+            color rgb(147, 153, 159)
+          .user
+            float right
+            text-align right
+            .name
+              line-height 12px
+              font-size 10px
+              color rgb(147, 153, 159)
+            img
+              width 12px
+              height 12px
+              border-radius 50%
+          .detail
+            .icon-thumb_up,
+            .icon-thumb_down
+              line-height 24px
+              font-size 12px
+              color rgb(147, 153, 159)
+            .icon-thumb_up
+              color rgb(0, 160, 220)
+            .text
+              line-height 16px
+              font-size 12px
+              color rgb(7, 17, 27)
+      .no-rating
+        padding 16px 0 32px 0
         font-size 12px
-        color rgb(77, 85, 93)
-        font-weight 200
-    
+        color rgb(147, 153, 159)
 </style>
